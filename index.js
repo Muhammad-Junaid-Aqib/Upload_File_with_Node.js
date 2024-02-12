2929df8e02eeb01c97d2ba4080b54074df0ed0e8
Muhammad-Junaid-Aqib <m.junaidaqib@gmail.com>
2024-02-12 23:26:23 +0500

    complete

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const multer = require("multer");
 
 const app = express();
 const PORT = 5000;
-const upload = multer({ dest: "uploads/" });
+// const upload = multer({ dest: "uploads/" });
 
 app.set("view engine", "ejs");
 app.set("views", path.resolve("./views"));
@@ -12,6 +12,17 @@ app.set("views", path.resolve("./views"));
 // app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+// use to store data in file
+const storage = multer.diskStorage({
+  destination: function (res, file, cb) {
+    return cb(null, "./uploads");
+  },
+  filename: function (res, file, cb) {
+    return cb(null, `${Date.now()}-${file.originalname}`);
+  },
+});
+
+const upload = multer({storage})
 app.get("/", (req, res) => {
   return res.render("homePage");
 });
@@ -19,9 +30,9 @@ app.get("/", (req, res) => {
 app.post("/uploads", upload.single("profileImg"), (req, res) => {
   //   return res.render("homePage");
   console.log(req.body);
-  console.log(req.body);
+  console.log(req.file);
 
-  return res.redirect('/')
+  return res.redirect("/");
 });
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
